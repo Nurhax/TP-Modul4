112d28c3cd3c52d2ba45c4de9e81e2424512cb01
Nurhax <iqbalnur2009@gmail.com>
2024-03-10 11:21:06 +0700

    menambahkan implementasi dengan table-driven

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class kodepos
+{
+    public enum EnumKelurahan
+    {
+        Batununggal, Kujangsari, Mengger, Wates, Cijaura,
+        Jatisari,Margasari,Sekejati,Kebonwaru,Maleer,Samoja
+    }
+    public static int getKodePos(EnumKelurahan inputKelurahan)
+    {
+        int[] kodePos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273};
+        return kodePos[(int)inputKelurahan];
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        kodepos.EnumKelurahan kelurahan = kodepos.EnumKelurahan.Cijaura;
+        int kodePosKelurahan = kodepos.getKodePos(kelurahan);
+        Console.WriteLine("Kelurahan: " +  kelurahan);
+        Console.WriteLine("Kodepos: " + kodePosKelurahan);
+    }
+}
